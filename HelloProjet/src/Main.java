a6c5a6433df781cdf457923442db147492e6401f
lpp <343061999@qq.com>
2019-06-24 06:01:17 +0800

    更改输出

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("HelloWorld!");
+        System.out.println("HelloWor555ld!");
     }
     @Test
     public void testHello() {

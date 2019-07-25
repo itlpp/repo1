596c9667f28c018cf3aa17c1ad5f63744add0887
lpp <343061999@qq.com>
2019-07-26 00:18:26 +0800

    测试修改q

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,8 +5,8 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("2527857");
-        System.out.println("2527857");
-        System.out.println("2527857");
+        System.out.println("搞47");
+        System.out.println("23份7857");
     }
     @Test
     public void testHello() {

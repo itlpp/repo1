2e2d84a933a870c13a5788bc3ee26111ef3a3d6c
lpp <343061999@qq.com>
2019-07-26 00:08:08 +0800

    测试修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("冲突");
+        System.out.println("2527857");
     }
     @Test
     public void testHello() {
